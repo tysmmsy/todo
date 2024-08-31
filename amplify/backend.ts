f40fd8315d4aafadf2809d5931250b425ce21b6b
tysmmsy <tysmmsy@gmail.com>
2024-08-31 23:08:57 +0900

    http api追加

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,55 @@
 import { defineBackend } from '@aws-amplify/backend'
+import { Stack } from 'aws-cdk-lib'
+import { HttpApi } from 'aws-cdk-lib/aws-apigatewayv2'
+import { Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam'
 import { auth } from './auth/resource'
 import { data } from './data/resource'
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+const backend = defineBackend({
 	auth,
 	data,
+})
+
+const apiStack = backend.createStack('api-stack')
+
+const httpApi = new HttpApi(apiStack, 'HttpApi', {
+	apiName: 'myHttpApi',
+	corsPreflight: {
+		allowMethods: [
+			// CorsHttpMethod.GET,
+			// CorsHttpMethod.POST,
+			// CorsHttpMethod.PUT,
+			// CorsHttpMethod.DELETE,
+		],
+		allowOrigins: ['*'],
+		allowHeaders: ['*'],
+	},
+	createDefaultStage: true,
+})
+
+const apiPolicy = new Policy(apiStack, 'ApiPolicy', {
+	statements: [
+		new PolicyStatement({
+			actions: ['execute-api:Invoke'],
+			resources: [
+				`${httpApi.arnForExecuteApi('*', '/todo')}`,
+				`${httpApi.arnForExecuteApi('*', '/todo/*')}`,
+			],
+		}),
+	],
+})
+
+backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(apiPolicy)
+
+backend.addOutput({
+	custom: {
+		API: {
+			// biome-ignore lint/style/noNonNullAssertion: <explanation>
+			[httpApi.httpApiName!]: {
+				endpoint: httpApi.url,
+				region: Stack.of(httpApi).region,
+				apiName: httpApi.httpApiName,
+			},
+		},
+	},
 })
