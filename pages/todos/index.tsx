6bf45bb6de9b0f61e724edb727e5f82bbe5e8b5a
tysmmsy <tysmmsy@gmail.com>
2024-09-01 12:53:08 +0900

    ログアウトボタン追加

--- pages/todos/index.tsx
+++ pages/todos/index.tsx
@@ -1,5 +1,15 @@
+import { Button } from '@mui/material'
+import { signOut } from 'aws-amplify/auth'
+
 const Todos = function Page() {
-	return <>サインイン成功</>
+	return (
+		<>
+			サインイン成功
+			<Button variant='contained' onClick={() => signOut()}>
+				ログアウト
+			</Button>
+		</>
+	)
 }
 
 export default Todos
