1c7ee45e6cae2329e62330a469286ea0201f872c
tysmmsy <tysmmsy@gmail.com>
2024-09-01 11:34:05 +0900

    - 不要なファイル削除 - 一旦サインアウトボタンだけ表示するように修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,15 @@ import outputs from '@/amplify_outputs.json'
 import { Amplify } from 'aws-amplify'
 import type { AppProps } from 'next/app'
 import '@aws-amplify/ui-react/styles.css'
+import { CssBaseline } from '@mui/material'
 import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter'
 
 Amplify.configure(outputs)
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<AppCacheProvider {...pageProps}>
+		<AppCacheProvider {...Component}>
+			<CssBaseline />
 			<Component {...pageProps} />
 		</AppCacheProvider>
 	)
