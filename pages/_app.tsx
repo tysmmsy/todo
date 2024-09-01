df4221dd22c722b72bbbbd118b5541177ed3c409
tysmmsy <tysmmsy@gmail.com>
2024-09-01 11:13:49 +0900

    - mui追加 - nextjs integration実施

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import outputs from '@/amplify_outputs.json'
 import { Amplify } from 'aws-amplify'
 import type { AppProps } from 'next/app'
 import '@aws-amplify/ui-react/styles.css'
+import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter'
 
 Amplify.configure(outputs)
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<AppCacheProvider {...pageProps}>
+			<Component {...pageProps} />
+		</AppCacheProvider>
+	)
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,30 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import {
+	DocumentHeadTags,
+	type DocumentHeadTagsProps,
+	documentGetInitialProps,
+} from '@mui/material-nextjs/v13-pagesRouter'
+import {
+	type DocumentContext,
+	Head,
+	Html,
+	Main,
+	NextScript,
+} from 'next/document'
 
-export default function Document() {
+export default function Document(props: DocumentHeadTagsProps) {
 	return (
-		<Html lang='en'>
+		<Html lang='ja'>
 			<Head />
+			<DocumentHeadTags {...props} />
 			<body>
 				<Main />
 				<NextScript />
 			</body>
 		</Html>
 	)
+}
+
+Document.getInitialProps = async (ctx: DocumentContext) => {
+	const finalProps = await documentGetInitialProps(ctx)
+	return finalProps
 }
