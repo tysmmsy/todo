2de25ea410c9b60ec24a7722a5579d3a8e85e6ae
tysmmsy <tysmmsy@gmail.com>
2024-09-04 22:58:32 +0900

    フロントエンドから呼び出し処理追加

--- pages/todos/index.tsx
+++ pages/todos/index.tsx
@@ -1,6 +1,6 @@
 import PageContainer from '@/layout/PageContainer'
 import { Box, Button, Typography } from '@mui/material'
-import { post, put } from 'aws-amplify/api'
+import { del, post, put } from 'aws-amplify/api'
 import { fetchAuthSession, signOut } from 'aws-amplify/auth'
 
 const postTodo = async () => {
@@ -51,7 +51,29 @@ const putTodo = async () => {
 		const { body } = await restOperation.response
 		const response = await body.json()
 
-		console.log('post call succeeded', response)
+		console.log('put call succeeded', response)
+	} catch (error) {
+		// TODO: コンポーネント化するときにエラーを表示するように
+		console.log(error)
+	}
+}
+
+const deleteTodo = async () => {
+	try {
+		const restOperation = del({
+			apiName: 'myHttpApi',
+			// 動作確認用
+			path: 'todo/01J6YBY13N6FM5KZPM9E39CE10',
+			options: {
+				headers: {
+					Authorization: `Bearer ${(await fetchAuthSession()).tokens?.accessToken}`,
+				},
+			},
+		})
+
+		const response = await restOperation.response
+
+		console.log('del call succeeded', response)
 	} catch (error) {
 		// TODO: コンポーネント化するときにエラーを表示するように
 		console.log(error)
@@ -71,6 +93,9 @@ const Todos = function Page() {
 				</Button>
 				<Button variant='contained' onClick={putTodo} sx={{ mt: 2 }}>
 					更新
+				</Button>
+				<Button variant='contained' onClick={deleteTodo} sx={{ mt: 2 }}>
+					削除
 				</Button>
 			</Box>
 		</PageContainer>
