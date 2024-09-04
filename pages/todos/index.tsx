9e644c17f3190c1e5ab44f9f216fc799dadea3c8
tysmmsy <tysmmsy@gmail.com>
2024-09-04 22:21:39 +0900

    フロントエンドから呼び出し処理追加

--- pages/todos/index.tsx
+++ pages/todos/index.tsx
@@ -1,6 +1,6 @@
 import PageContainer from '@/layout/PageContainer'
 import { Box, Button, Typography } from '@mui/material'
-import { post } from 'aws-amplify/api'
+import { post, put } from 'aws-amplify/api'
 import { fetchAuthSession, signOut } from 'aws-amplify/auth'
 
 const postTodo = async () => {
@@ -30,6 +30,34 @@ const postTodo = async () => {
 	}
 }
 
+const putTodo = async () => {
+	try {
+		const restOperation = put({
+			apiName: 'myHttpApi',
+			// 動作確認用
+			path: 'todo/01J6YBY13N6FM5KZPM9E39CE10',
+			options: {
+				headers: {
+					Authorization: `Bearer ${(await fetchAuthSession()).tokens?.accessToken}`,
+				},
+				body: {
+					// TODO: UIから入力できるようにする
+					title: 'test update title',
+					content: 'test update content'.repeat(1000),
+				},
+			},
+		})
+
+		const { body } = await restOperation.response
+		const response = await body.json()
+
+		console.log('post call succeeded', response)
+	} catch (error) {
+		// TODO: コンポーネント化するときにエラーを表示するように
+		console.log(error)
+	}
+}
+
 const Todos = function Page() {
 	return (
 		<PageContainer title='todo'>
@@ -40,6 +68,9 @@ const Todos = function Page() {
 				</Button>
 				<Button variant='contained' onClick={postTodo} sx={{ mt: 2 }}>
 					追加
+				</Button>
+				<Button variant='contained' onClick={putTodo} sx={{ mt: 2 }}>
+					更新
 				</Button>
 			</Box>
 		</PageContainer>
